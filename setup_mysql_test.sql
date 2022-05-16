9aea7f9c8940489fba52d0bfdca8c3004e14da56
Byakko12 <davidlandg@gmail.com>
2022-05-15 19:30:11 -0500

    FIX - mysqltest file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create user hbnb_test'@'localhost with password hbnb_dev_pwd
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- privileges on hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_test. * TO 'hbnb_test'@'localhost';
 -- SELECT PRIVILIGE on performance_schema db
 GRANT SELECT ON performance_schema. * TO 'hbnb_test'@'localhost';
+-- privileges on hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test. * TO 'hbnb_test'@'localhost';
