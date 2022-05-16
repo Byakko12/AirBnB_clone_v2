a7a8ef9aa5d618bcdc86e939a0014bb0b56445d9
Byakko12 <davidlandg@gmail.com>
2022-05-15 19:05:19 -0500

    [FIX] - comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,8 @@
+-- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create user hbnb_dev'@'localhost with password hbnb_dev_pwd
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- privileges on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
+-- SELECT PRIVILIGE on performance_schema db
 GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
