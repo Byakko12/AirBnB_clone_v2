974fb1aa804afb1e8eebcad4ab16ee493ed4aeed
Byakko12 <davidlandg@gmail.com>
2022-05-15 19:45:04 -0500

    [FIX] - select privileges

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,8 @@
 -- create database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create user hbnb_test'@'localhost with password hbnb_dev_pwd
+-- create user hbnb_test'@'localhost with password hbnb_test_pwd
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- SELECT PRIVILIGE on performance_schema db
 GRANT SELECT ON performance_schema. * TO 'hbnb_test'@'localhost';
 -- privileges on hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_test. * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test. * TO 'hbnb_test_db '@'localhost';
